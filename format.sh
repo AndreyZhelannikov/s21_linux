93ab4dd2d1a8a995e818493a864cabcec0817ba3
AndreyZhelannikov <zhelannikovAndrey@gmail.com>
2022-05-23 16:51:32 +0300

    fix

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for file in *.md;do
-    sed -ri 's|(\!\[\[Pasted\ image\ )|![](https://github.com/AndreyZhelannikov/s21_linux/blob/develop/Attachmnts/Pasted%20image%20|g' "$file";
+    sed -ri 's|(\!\[\[Pasted\ image\ )|![](https://github.com/AndreyZhelannikov/s21_linux/blob/develop/Attachments/Pasted%20image%20|g' "$file";
     sed -ri 's|(\]\])|)|g' "$file";
 done;
